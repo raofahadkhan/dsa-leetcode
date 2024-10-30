279000586b548220d84f464fd32a62d0f14227b8
Fahad Khan <raofahad046@gmail.com>
2024-10-30 14:15:38 +0500

    Time: 0 ms (100%), Space: 16.5 MB (95.53%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -1,16 +1,14 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        close_paren = {')': '(', '}': '{', ']': '['}
+        close_paren = {')':'(', '}':'{', ']':'['}
 
         for ch in s:
-            if ch in close_paren.values():  # if ch is an opening bracket
+            if ch in '({[':
                 stack.append(ch)
-            elif ch in close_paren:  # if ch is a closing bracket
-                top_element = stack.pop() if stack else '#'
-                if close_paren[ch] != top_element:
-                    return False
+            elif stack and close_paren[ch] == stack[-1]:
+                stack.pop()
             else:
-                return False  # invalid character
+                return False
 
-        return not stack  # stack should be empty if all brackets are balanced
+        return True if not stack else False
