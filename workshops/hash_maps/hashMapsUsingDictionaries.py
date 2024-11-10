7bc500a983f6644bfc1671581fe98cfe692657c3
Fahad Khan <raofahad046@gmail.com>
2024-11-10 19:01:41 +0500

    learned how to implement hash maps using lists and dictionaries in python

---
+++ workshops/hash_maps/hashMapsUsingDictionaries.py
@@ -0,0 +1,38 @@
+# IMPLEMENTATION OF HASH_MAPS USING DICTIONARIES
+
+class PhoneBook:
+    def __init__(self):
+        self.phone_book = {}
+        
+    def add_contact(self, contact_name, contact_number):
+        self.phone_book[contact_name] = contact_number
+        
+    def get_contact(self, contact_name):
+        
+        # 1st Approach
+        # return self.phone_book[contact_name] if contact_name in self.phone_book else "Contact Number Not Found"
+    
+        # 2nd Approach
+        return self.phone_book.get(contact_name, "Contact Number Not Found")
+    
+    def __repr__(self):
+    # Provide a readable representation of the object
+        return f"PhoneBook({self.phone_book})"
+    
+contact_list = PhoneBook()
+contact_list.add_contact("fahad", "+92312-0000000")
+print(contact_list)
+
+contact_list.add_contact("ali", "+92312-0000000")
+print(contact_list)
+
+ali_contact_number = contact_list.get_contact("ali")
+print("Ali's Contact Number: " + ali_contact_number)
+
+fahad_contact_number = contact_list.get_contact('fahad')
+print("Fahad's Contact Number: " + fahad_contact_number)
+
+#  GETTING THE CONTACT NUMBER WHICH DOESN'T EXISTS IN CONTACT_LIST
+
+hamza_contact_number = contact_list.get_contact('hamza')
+print("Hamza's Contact Number: " + hamza_contact_number)
---
+++ workshops/hash_maps/hashMapsUsingLists.py
@@ -0,0 +1,35 @@
+# IMPLEMENTATION OF HASH MAP USING LISTS:-
+
+class StudentScores:
+    
+    def __init__(self, size = 10):
+        self.size = size
+        self.scores = [None] * size
+        
+    def hash_function(self, key):
+        return hash(key) % self.size
+    
+    def add_score(self, student_name, score):
+        index = self.hash_function(student_name)
+        self.scores[index] = (student_name, score)
+        
+    def get_score(self, student_name):
+        index = self.hash_function(student_name)
+        return self.scores[index]
+    
+    def __repr__(self):
+        # Provide a readable representation of the object
+        return f"StudentScores({self.scores})"
+    
+score_list = StudentScores()
+print(score_list)
+score_list.add_score("fahad", 50)
+print(score_list)
+score_list.add_score("faizan", 100)
+print(score_list)
+
+faizan_score = score_list.get_score("faizan")
+print(faizan_score)
+
+fahad_score = score_list.get_score("fahad")
+print(fahad_score)
